f2a375403ba0cd0a9420c85b31892c93b2630470
confusion <yangmingcheng@matrixone.io>
2024-04-22 15:05:14 +0800

    使用asyncio 超时控制

--- core.py
+++ core.py
@@ -97,8 +97,8 @@ class AsyncGrassWs:
                     ws_proxy = socks.socksocket()
                     ws_proxy.set_proxy(socks.PROXY_TYPES[proxy_type.upper()], http_proxy_host, http_proxy_port,
                                        username=username, password=password)
-                    ws_proxy.settimeout(self.proxy_timeout)
-                    await loop.run_in_executor(None, ws_proxy.connect, (self.server_hostname, self.server_port))  # 执行阻塞函数
+                    async with asyncio.timeout(self.proxy_timeout):
+                        await loop.run_in_executor(None, ws_proxy.connect, (self.server_hostname, self.server_port))  # 执行阻塞函数
                     self.log(DEBUG, f'[连接代理成功] [{self.user_id}] [{self.proxy_url}]')
                 ssl_context = ssl.create_default_context()
                 ssl_context.check_hostname = False
@@ -131,6 +131,8 @@ class AsyncGrassWs:
                         pong_response = {"id": message["id"], "origin_action": "PONG"}
                         self.log(DEBUG, f'[发送消息] [{self.user_id}] [{self.proxy_url}] [{pong_response}]')
                         await self.ws.send(json.dumps(pong_response))
+            except TimeoutError as e:
+                self.log(INFO, f'[连接超时] [{self.user_id}] [{self.proxy_url}] {e}')
             except Exception as e:
                 self.log(INFO, f'[连接断开] [{self.user_id}] [{self.proxy_url}] {e}')
             self.status = Status.disconnect
